0997a20fd09238d6342e28f72b6b870f9ef67dcc
Sören Reichardt <soren.reichardt@neotechnology.com>
2023-05-05 16:15:49 +0200

    Implement binary operators

--- src/compiler.rs
+++ src/compiler.rs
@@ -10,6 +10,7 @@ use miette::{NamedSource, Result};
 pub(crate) struct Compiler<'a> {
     parser: Parser,
     source: &'a str,
+    scanner: Scanner<'a>
 }
 
 #[derive(Default)]
@@ -18,28 +19,47 @@ pub(crate) struct Parser {
     current: Option<Token>,
 }
 
-impl<'a> Compiler<'a> {
+#[repr(u8)]
+#[derive(Clone)]
+enum Precedence {
+    None,
+    Assignment,
+    Or,
+    And,
+    Equality,
+    Comparison,
+    Term,
+    Factor,
+    Unary,
+    Call,
+    Primary,
+}
+
+struct ParseRule {
+    prefix_fn: fn() -> (),
+    infix_fn: Option<fn() -> ()>,
+    precedence: Precedence,
+}
+
+impl<'a,> Compiler<'a> {
     pub(crate) fn new(parser: Parser, source: &'a str) -> Self {
-        Compiler { parser, source }
+        Compiler { parser, source, scanner: Scanner::new(source) }
     }
 
     pub(crate) fn compile(&mut self, chunk: &mut Chunk) -> Result<()> {
-        let scanner = Scanner::new(self.source);
-        let mut token_iterator = scanner.into_iter();
-
-        self.advance(&mut token_iterator)?;
+        self.advance()?;
         self.expression()?;
-        self.consume(TokenType::Eof, &mut token_iterator)?;
+        self.consume(TokenType::Eof)?;
 
         self.end_compiler(chunk);
         Ok(())
     }
 
-    fn advance<I: Iterator<Item = Result<Token>>>(&mut self, token_iterator: &mut I) -> Result<()> {
+    fn advance(&mut self) -> Result<()> {
         self.parser.previous = self.parser.current.take();
 
         loop {
-            match token_iterator.next() {
+            match self.scanner.next() {
                 Some(Ok(token)) => {
                     self.parser.current = Some(token);
                     break;
@@ -54,13 +74,12 @@ impl<'a> Compiler<'a> {
         Ok(())
     }
 
-    fn consume<I: Iterator<Item = Result<Token>>>(
+    fn consume(
         &mut self,
         expected_type: TokenType,
-        token_iterator: &mut I
     ) -> Result<()> {
         if self.parser.current.as_ref().map(|token| &token.tpe) == Some(&expected_type) {
-            self.advance(token_iterator)?;
+            self.advance()?;
             return Ok(());
         } else {
             let current_token = self.parser.current.take().unwrap();
@@ -77,29 +96,53 @@ impl<'a> Compiler<'a> {
         self.emit_return(chunk)
     }
 
-    fn number(&self, chunk: &mut Chunk) {
+    fn expression(&self) -> Result<()> {
+        self.parse_precedence(Precedence::Assignment);
+        todo!()
+    }
+
+    fn number(&self, chunk: &mut Chunk) -> Result<()> {
         let previous = self.parser.previous.as_ref().expect("No previous value");
         let string_value = &self.source[previous.start..previous.start + previous.length];
-        println!("here {}", string_value);
         let value = string_value.parse::<Value>().unwrap();
-    
+
         self.emit_constant(value as Value, chunk)
     }
 
-    fn grouping<I: Iterator<Item = Result<Token>>>(&mut self, token_iterator: &mut I) -> Result<()> {
+    fn grouping(&mut self) -> Result<()> {
         self.expression()?;
-        self.consume(TokenType::RightParen, token_iterator)
+        self.consume(TokenType::RightParen)
     }
 
     fn unary(&self, chunk: &mut Chunk) -> Result<()> {
-        let op_type = &self.parser.previous.as_ref().expect("No value present").tpe;
-        self.expression()?;
+        let op_type = &self.previous().tpe;
+        self.parse_precedence(Precedence::Unary);
 
         match op_type {
             TokenType::Minus => self.emit_byte(OpCode::OpSubtract as u8, chunk),
-            _ => ()
+            _ => (),
         };
         Ok(())
+    }
+
+    fn binary(&self, chunk: &mut Chunk) -> Result<()> {
+        let operator_type = &self.previous().tpe;
+        let rule = self.get_rule(operator_type);
+        self.parse_precedence(rule.precedence.next());
+        match operator_type {
+            TokenType::Plus => self.emit_byte(OpCode::OpAdd as u8, chunk),
+            TokenType::Minus => self.emit_byte(OpCode::OpSubtract as u8, chunk),
+            TokenType::Star => self.emit_byte(OpCode::OpMultiply as u8, chunk),
+            TokenType::Slash => self.emit_byte(OpCode::OpDivide as u8, chunk),
+            _ => (),
+        };
+        Ok(())
+    }
+
+    fn parse_precedence(&self, precedence: Precedence) {}
+
+    fn get_rule(&self, operator_type: &TokenType) -> ParseRule {
+        todo!()
     }
 
     fn emit_byte(&self, byte: u8, chunk: &mut Chunk) {
@@ -122,15 +165,17 @@ impl<'a> Compiler<'a> {
         self.emit_byte(OpCode::OpReturn as u8, chunk)
     }
 
-    fn emit_constant(&self, value: Value, chunk: &mut Chunk) {
-        self.emit_bytes(OpCode::OpConstant as u8, self.make_constant(value, chunk)?, chunk)
+    fn emit_constant(&self, value: Value, chunk: &mut Chunk) -> Result<()> {
+        let constant_position = self.make_constant(value, chunk)?;
+        self.emit_bytes(OpCode::OpConstant as u8, constant_position, chunk);
+        Ok(())
     }
 
     fn make_constant(&self, value: Value, chunk: &mut Chunk) -> Result<u8> {
         let constant_position = chunk.add_constant(value);
         match constant_position {
             u8::MAX => {
-                let previous = self.parser.previous.as_ref().expect("No previous value");
+                let previous = self.previous();
                 Err(CompileError {
                     msg: "Too many constants in one chunk.".to_owned(),
                     src: NamedSource::new("", self.source.to_owned()),
@@ -142,7 +187,39 @@ impl<'a> Compiler<'a> {
         }
     }
 
-    fn expression(&self) -> Result<()> {
-        todo!()
+    fn current(&self) -> &Token {
+        self.parser.current.as_ref().expect("No value present")
+    }
+
+    fn previous(&self) -> &Token {
+        self.parser.previous.as_ref().expect("No value present")
+    }
+}
+
+impl Precedence {
+    fn next(&self) -> Precedence {
+        let enum_value: u8 = self.clone() as u8;
+        (enum_value + 1).try_into().unwrap()
+    }
+}
+
+impl TryFrom<u8> for Precedence {
+    type Error = String;
+
+    fn try_from(value: u8) -> std::result::Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Precedence::None),
+            1 => Ok(Precedence::Assignment),
+            2 => Ok(Precedence::Or),
+            3 => Ok(Precedence::And),
+            4 => Ok(Precedence::Equality),
+            5 => Ok(Precedence::Comparison),
+            6 => Ok(Precedence::Term),
+            7 => Ok(Precedence::Factor),
+            8 => Ok(Precedence::Unary),
+            9 => Ok(Precedence::Call),
+            10 => Ok(Precedence::Primary),
+            _ => Err("unknown value".to_string()),
+        }
     }
 }
